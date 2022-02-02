dd20e396297d19ab2ab32ddb0e3c2738c6a8156a
cosmin <N00201120@iadt.ie>
2022-02-02 01:12:30 +0000

    adding more

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
-clap(3,8);
-function clap(startNum,endNum){
-    let loopCount = endNum-startNum
-    for(let i=0;i<=num;i++){
-    console.log("hi"+ startNum+i)
+let distance =  23;
+let rectX = 20;
+let rectY =20;
+let rectWidth =50;
+let rectHeight =50;
+let numBoxs = 5;
+let spacing = 5;
+
+clap(69,420);
+
+function setup(){
+    createCanvas(500,500);
+    background(0);
+}
+
+function draw(){
+    fill(255,140,0);
+    noStroke();
+    drawBoxs();
+}
+
+function drawBoxs(){
+    for(let i = 0; i<numBoxs; i++){
+        let totalSpacing = rectWidth + spacing;
+        rect(i*totalSpacing + rectX, rectY, rectWidth, rectHeight);
     }
+}
+
+function clap(startnum, endnum){
+    let loopCount = endnum - startnum;
+    for(let i=0; i<=loopCount; i++){
+    console.log("hi again" + ( startnum+i))
+    }
+}
+
+function addMeUp(num01,num02){
+    let total = num01 + num02;
+    console.log("job done");
+    return total;
 }
